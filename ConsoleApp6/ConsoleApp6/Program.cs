e632107ef8604d3c6cd3ae6baf0f2dee1b203817
javid20afg <jasa98002@utb.vaxjo.se>
2019-02-15 16:15:18 +0100

    projekt

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namn;
+        
+            Console.WriteLine("vad är ditt namn?");
+            namn = Console.ReadLine();
+            Console.WriteLine("vad är ditt efternamn*?");
+            namn += Console.ReadLine();
+            Console.WriteLine(namn);
+
+            Console.ReadLine();
+        }
+    }
+}
